2eae9c45bf4857c54bc8c3302b2deb771cfcdca1
gasparro1125 <albertogasparrobonett1@gmail.com>
2021-05-25 14:24:10 +0200

    margi

--- src/01 deliverables/exercise-01.js
+++ src/01 deliverables/exercise-01.js
@@ -1,1 +1,18 @@
 console.log("************** DELIVERABLE 01 *********************");
+
+//Head
+const head = (first,..arr) => first;
+console.log( "Head: " head([4,"b","c","d","e"]));
+
+//Tail
+const tail = ([first,...arr]) => arr;
+console.log( "tail: " tail([4,"b","c","d","e"]));
+
+
+//init
+const init = (arr) => arr.slice(0,arr.length-1);
+console.log( "last: " init([4,"b","c","d","e"]));
+
+//last
+const last = (arr) => arr.pop();
+console.log( "init: " last([4,"b","c","d","e",5]));
